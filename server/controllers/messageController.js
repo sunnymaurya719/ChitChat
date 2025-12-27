57391818ccad5cf26352bec5988270a348685529
sunnymaurya719 <sunnymaurya1920@gmail.com>
2025-12-27 20:23:43 +0530

    completed

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -7,7 +7,6 @@ import User from "../models/User.js";
 export const getUsersForSidebar  = async(req,res) => {
     try{
         const userId = req.user._id;
-        console.log(userId);
         const filteredUsers = await User.find({_id:{$ne: userId}}).select("-password");
         //Count number of messages not seen
         const unseenMessages = {}
