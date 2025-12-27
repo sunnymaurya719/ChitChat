48877bb5ea3ecde6d53b274a1e98199904320d73
sunnymaurya719 <sunnymaurya1920@gmail.com>
2025-12-27 21:01:08 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React,{useContext} from 'react'
 import {Navigate, Route,Routes} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
-import assets from './assets/assets.js'
 import ProfilePage from './pages/ProfilePage'
 import { Toaster } from 'react-hot-toast'
 import { AuthContext } from '../context/AuthContext.jsx'
@@ -10,7 +9,7 @@ import { AuthContext } from '../context/AuthContext.jsx'
 const App = () => {
   const {authUser} = useContext(AuthContext);
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster />
       <Routes>
         <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />}/>
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import "dotenv/config";
 import cors from 'cors';
 import http from 'http';
@@ -13,26 +13,26 @@ const server = http.createServer(app);
 
 //Initialise socket.io server
 export const io = new Server(server, {
-    cors: {origin: "*"}
+    cors: { origin: "*" }
 })
 
 //Store online users
 export const userSocketMap = {}; //{userId: socketId}
 
 //Socket.io handler
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
-    console.log("User connected",userId);
+    console.log("User connected", userId);
 
-    if(userId) userSocketMap[userId] = socket.id;
+    if (userId) userSocketMap[userId] = socket.id;
 
     //Emit online users to all connected clients
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect", ()=>{
-        console.log("User Disconnected",userId);
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", userId);
         delete userSocketMap[userId];
-        io.emit("getOnlineUsers",Object.keys(userSocketMap))
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
     })
 })
 
@@ -40,17 +40,22 @@ io.on("connection",(socket)=>{
 await connectDB();
 
 //Middlware setup
-app.use(express.json({limit:"4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 //Routes setup
-app.get("/",(req,res)=>{ res.send("Server is live") })
-app.use("/api/auth",userRouter);
-app.use("/api/messages",messageRouter)
+app.get("/", (req, res) => { res.send("Server is live") })
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter)
 
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => {
+        console.log("Server is running on port : ", PORT);
+    })
+}
 
-// server.listen(PORT,()=>{
-//     console.log("Server is running on port : ",PORT);
-// })
+//Export server for vercel
+export default server;
+
